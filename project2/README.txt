62198124d341b4a4238ae059078eaf2f53fd1d86
Daniel Van Allen <daniel@danielvanallen.org>
2013-10-31 19:36:06 -0500

    began project2

---
+++ project2/README.txt
@@ -0,0 +1,1 @@
+This project uses semaphores to control the flow of threads through a virtual DMV.
---
+++ project2/dmv_threads.c
@@ -0,0 +1,9 @@
+/* Daniel Van Allen
+ * CE4348.001: OS Concepts
+ * Project 2: DMV Threads
+ * Due: 2013-11-09
+ */
+
+#include <stdio.h>;
+
+/* Initialize Threads */
