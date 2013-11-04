c1f9da1ced3575483e74037c3800fc320a650f28
Daniel Van Allen <daniel@danielvanallen.org>
2013-11-04 02:48:58 -0600

    passed first thread test

--- project2/project2.c
+++ project2/project2.c
@@ -9,23 +9,9 @@
 
 #define MAX_CUSTOMERS 100
 
-/* Initialize Threads */
-int main() {
-	pthread_t customerThreads[MAX_CUSTOMERS];
-	for(int x = 0; x < MAX_CUSTOMERS; x++) {
-		int err = pthread_create(&customerThreads[x], NULL, &customer, (void*)id);
-		if(err != 0) {
-			printf("Error creating thread #%d\n", x);
-			pthread_exit(NULL);
-		}
-	}
-	
-	pthread_exit(NULL);
-}
-
 void *customer(void *arg) {
-	int id = arg;
-	printf("Created thread #%\n", id);
+	int id = (int)arg;
+	printf("Created thread #%d\n", id);
 }
 
 void *informationDesk(void *arg) {
@@ -39,3 +25,18 @@ void *announcer(void *arg) {
 void *agent(void *arg) {
 
 }
+
+int main() {
+        pthread_t customerThreads[MAX_CUSTOMERS];
+        int x;
+	for(x = 0; x < MAX_CUSTOMERS; x++) {
+                int err = pthread_create(&customerThreads[x], NULL, &customer, (void*)x);
+                if(err != 0) {
+                        printf("Error creating thread #%d\n", x);
+                        pthread_exit(NULL);
+                }
+        }
+        
+        pthread_exit(NULL);
+}
+
