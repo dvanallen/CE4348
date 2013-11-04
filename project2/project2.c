7b45ce27c9ecfa1baab827d5e29152f0614ae98d
Daniel Van Allen <daniel@danielvanallen.org>
2013-11-04 02:22:39 -0600

    updated design

--- project2/project2.c
+++ project2/project2.c
@@ -4,6 +4,38 @@
  * Due: 2013-11-09
  */
 
-#include <stdio.h>;
+#include <stdio.h>
+#include <pthread.h>
+
+#define MAX_CUSTOMERS 100
 
 /* Initialize Threads */
+int main() {
+	pthread_t customerThreads[MAX_CUSTOMERS];
+	for(int x = 0; x < MAX_CUSTOMERS; x++) {
+		int err = pthread_create(&customerThreads[x], NULL, &customer, (void*)id);
+		if(err != 0) {
+			printf("Error creating thread #%d\n", x);
+			pthread_exit(NULL);
+		}
+	}
+	
+	pthread_exit(NULL);
+}
+
+void *customer(void *arg) {
+	int id = arg;
+	printf("Created thread #%\n", id);
+}
+
+void *informationDesk(void *arg) {
+
+}
+
+void *announcer(void *arg) {
+
+}
+
+void *agent(void *arg) {
+
+}
